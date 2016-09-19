b36ad0e2281a9caf53765b1fa08fe577afff978e
Cody <cody.kendall21@gmail.com>
2016-09-19 14:05:32 -0700

    add javascript for scrolling

---
+++ javascript/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+	scrollToSchedule();
+	scrollToRegistration();
+	scrollToSponsors();
+	scrollToResults();
+	scrollToLodging();
+	scrollToVendors();
+
+})
+
+
+var scrollToSchedule = function(){
+	$('.schedule-link').on("click",function(){
+		$('html, body').animate({
+			scrollTop: $('.schedule').offset().top
+		}, 'slow')
+	})
+	
+}
+
+var scrollToRegistration = function(){
+	$('.registration-link').on("click",function(){
+		$('html, body').animate({
+			scrollTop: $('.registration').offset().top
+		}, 'slow')
+	})
+	
+}
+
+var scrollToSponsors = function(){
+	$('.sponsors-link').on("click",function(){
+		$('html, body').animate({
+			scrollTop: $('.sponsors').offset().top
+		}, 'slow')
+	})
+	
+}
+
+var scrollToResults = function(){
+	$('.results-link').on("click",function(){
+		$('html, body').animate({
+			scrollTop: $('.results').offset().top
+		}, 'slow')
+	})
+	
+}
+
+
+var scrollToLodging = function(){
+	$('.schedule-link').on("click",function(){
+		$('html, body').animate({
+			scrollTop: $('.lodging').offset().top
+		}, 'slow')
+	})
+	
+}
+
+
+var scrollToVendors = function(){
+	$('.vendors-link').on("click",function(event){
+		$('html, body').animate({
+			scrollTop: $('.vendors').offset().top
+		}, 'slow')
+	})
+	
+}
